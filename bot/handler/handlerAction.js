d26ce9f212c0924fe7c5a2fbe41ec318225b489c
MinatoCodes <subinsirmal11@gmail.com>
2025-05-18 08:09:09 +0545

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -44,7 +44,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
         onReaction();
 
                 if(event.reaction == "😡"){
-  if(event.userID == "100088286122703" ){
+  if(event.userID == "61575868051148" ){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -54,7 +54,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "🤍"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100088286122703"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61575868051148"){
     message.unsend(event.messageID)
 }else{
     message.send("")
